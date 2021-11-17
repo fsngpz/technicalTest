2d86e8e9f4c95fef9b71ca0c86639c2415b9c35f
fsngpz <91114367+fsngpz@users.noreply.github.com>
2021-11-17 09:34:52 +0700

    Update addticket.php

--- API/addticket.php
+++ API/addticket.php
@@ -10,6 +10,11 @@
         $tn = strval(uniqid('TN-'));
         $uuid = strval(md5(('time') . mt_rand(1,400)));
 
+        if (empty($Subject) || empty($Message) || empty($Priority)){
+          echo json_encode ("The value of parameter cannot be empty!");
+          return;
+        }
+
         if ($Priority != 'Low' && $Priority != 'Medium' && $Priority != 'High'){
             echo json_encode('Please input the Priority with Low, Medium or High');
         }else{
@@ -17,10 +22,10 @@
             $query = "INSERT INTO ticketing(ID,TicketNum,Subject,Message,Status,Priority,Create_Time,Update_Time) VALUES
             ('$uuid','$tn','$Subject','$Message','Open','$Priority', NOW(), NOW())";
             $sql = mysqli_query($db_connect, $query);
-            
+
             $query = "INSERT INTO history VALUES('$uuid',NOW(),'$Message')";
             $sql = mysqli_query($db_connect, $query);
-        
+
             if($sql){
                 echo json_encode(array('Message' => 'Success to adding the ticket'));
             }else{
@@ -28,8 +33,8 @@
             }
 
         }
-    
-        
+
+
     } else{
         echo json_encode('The Parameter is not valid. Please input the valid Parameter');
     }
